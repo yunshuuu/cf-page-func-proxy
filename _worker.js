854db1f910d1aef3c21538d99822860d246a8692
yunshuuu <105863176+yunshuuu@users.noreply.github.com>
2022-07-31 13:20:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="tiant.net";
+        url.hostname="127.0.0.1";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
