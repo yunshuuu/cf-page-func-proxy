45fe01a13f7c28534d346f0a136e236244b5e770
yunshuuu <105863176+yunshuuu@users.noreply.github.com>
2022-07-31 13:21:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="127.0.0.1";
+        url.hostname="127.0.0.1.sslip.io:32500";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
